9a37de80443aad99e346e818585cda659f41ae62
rajesha maravanthe <rraajjpoojary@gmail.com>
2017-03-14 10:35:38 +0530

    added new files

---
+++ app.js
@@ -0,0 +1,12 @@
+
+var person = {
+    name: "Rajesha Maravanthe",
+    age: "28",
+    dateofbirth: "05th sep 1987",
+    placeofbirth: "Kundapura"
+}
+
+console.log("Name: "+ person.name);
+console.log("Age: "+ person.age);
+console.log("DOB: "+ person.dateofbirth);
+console.log("Place: "+ person.placeofbirth);
